3ee07a02a76baaea68e53ccd9dc8cfb46469f34e
Brother Nelson <nelsoned@byui.edu>
2023-03-15 23:06:09 +0000

    Add client side routing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import { createElement } from './utils';
+import { initRouter } from './router';
 
-import Counter from './Counter';
-
-function App() {
-  const header = createElement('h1', {
+function Header(mainDiv) {
+  const appTitle = createElement('h1', {
     textContent: 'My Cool Project',
     className: 'heading',
   });
 
-  const main = createElement('main', {}, [Counter()]);
+  // nav items
+  const page1 = createElement('a', {
+    href: '/#/page1',
+    textContent: 'Page 1',
+  });
+  const page2 = createElement('a', {
+    href: '/#/page2',
+    textContent: 'Page 2',
+  });
+  const page3 = createElement('a', {
+    href: '/#/page3',
+    textContent: 'Page 3',
+  });
 
-  return createElement('div', {}, [header, main]);
+  const nav = createElement('nav', {}, [page1, page2, page3]);
+
+  return createElement('header', {}, [appTitle, nav]);
+}
+
+function Footer() {
+  const copyright = createElement('span', {
+    textContent: `Copyright © ${new Date().getFullYear()}`,
+  });
+
+  return createElement('footer', {}, [copyright]);
+}
+
+function App() {
+  const main = createElement('main', {}, []);
+
+  initRouter(main);
+
+  return createElement('div', {}, [Header(main), main, Footer()]);
 }
 
 export default App;
